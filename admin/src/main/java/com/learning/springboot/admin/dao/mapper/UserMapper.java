0bbac96ae7331f12bacc2c607be37598fba5e54b
EdmitSpera <laoxiao1886@gmail.com>
2024-10-10 14:48:08 +0800

    feature:mapper声明

---
+++ admin/src/main/java/com/learning/springboot/admin/dao/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.learning.springboot.admin.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.learning.springboot.admin.dao.entity.UserDo;
+
+public interface UserMapper extends BaseMapper<UserDo> {
+}
