ffe933742d328924b5f2502118b858b9c3219c5f
EdmitSpera <laoxiao1886@gmail.com>
2024-10-10 14:48:31 +0800

    feature:用户后管实体Do

---
+++ admin/src/main/java/com/learning/springboot/admin/dao/entity/ExcelReadUserDo.java
@@ -0,0 +1,52 @@
+package com.learning.springboot.admin.dao.entity;
+
+import lombok.Data;
+
+@Data
+public class ExcelReadUserDo {
+    /**
+     * 学号
+     */
+    private String studentNumber;
+
+    /**
+     * 真实姓名
+     */
+    private String realName;
+
+    /**
+     * 户外ID
+     */
+    private String nickname;
+
+    /**
+     * 性别
+     */
+    private String gender;
+
+    /**
+     * 部门
+     */
+    private String department;
+
+    /**
+     * 年级
+     */
+    private int grade;
+
+    /**
+     * 专业
+     */
+    private String major;
+
+    /**
+     * 校内邮箱
+     */
+    private String mail;
+
+    /**
+     * 电话
+     */
+    private String phone;
+
+}
