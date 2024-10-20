d6a0ddefdda507a950aca73c353b4b658dea50a5
EdmitSpera <laoxiao1886@gmail.com>
2024-10-20 11:07:24 +0800

    delete：删除或移动某些类

--- admin/src/main/java/com/learning/springboot/admin/common/constants/RedisCacheConstant.java
+++
@@ -1,7 +0,0 @@
-package com.learning.springboot.admin.common.constants;
-
-public class RedisCacheConstant {
-    public static String LOCK_USER_REGISTER_KEY = "sloc:lock_user-register";
-
-    public static String ADMIN_USER_KEY = "sloc:admin-user:";
-}
