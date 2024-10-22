12a21295a61bc0c732f99b0e5e5407d15ec9c815
EdmitSpera <laoxiao1886@gmail.com>
2024-10-22 20:16:50 +0800

    rebuild：优化数据库配置，自动注入

--- framework/src/main/java/com/learning/springboot/framework/config/DataBaseConfiguration.java
+++ framework/src/main/java/com/learning/springboot/framework/config/DataBaseConfiguration.java
@@ -1,11 +1,15 @@
 package com.learning.springboot.framework.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Date;
 
 @Configuration
 public class DataBaseConfiguration {
@@ -19,5 +23,31 @@ public class DataBaseConfiguration {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         return interceptor;
+    }
+
+    /**
+     * MyBatis-Plus 源数据自动填充类
+     */
+    @Bean
+    public MyMetaObjectHandler myMetaObjectHandler() {
+        return new MyMetaObjectHandler();
+    }
+
+    /**
+     * 字段填充处理器
+     */
+    static class MyMetaObjectHandler implements MetaObjectHandler {
+        @Override
+        public void insertFill(MetaObject metaObject) {
+            strictInsertFill(metaObject, "createTime", Date::new, Date.class);
+            strictInsertFill(metaObject, "modifyTime", Date::new, Date.class);
+            strictInsertFill(metaObject, "dailyBonus", () -> 0, Integer.class);
+            strictInsertFill(metaObject, "projectBonus", () -> 0, Integer.class);
+            strictInsertFill(metaObject, "delFlag", () -> 0, Integer.class);
+        }
+        @Override
+        public void updateFill(MetaObject metaObject) {
+            strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+        }
     }
 }
